8ac4caee64b05cb97d7c48cb58758b51d7a74781
Jonathan Gabriel Garay <103967649+Jonise1564@users.noreply.github.com>
2025-10-11 02:52:16 -0300

    inicio Mapa

--- app/src/main/java/com/example/inmobiliaria/ui/inicio/InicioFragment.java
+++ app/src/main/java/com/example/inmobiliaria/ui/inicio/InicioFragment.java
@@ -1,23 +1,35 @@
 package com.example.inmobiliaria.ui.inicio;
 
 import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.inmobiliaria.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class InicioFragment extends Fragment {
 
     private InicioViewModel mViewModel;
 
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng ubInmo = new LatLng(-33.2950, -66.3356); // San Luis Capital
+            googleMap.addMarker(new MarkerOptions().position(ubInmo).title("Inmobiliaria LP"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubInmo, 14));
+        }
+    };
     public static InicioFragment newInstance() {
         return new InicioFragment();
     }
@@ -29,10 +41,18 @@ public class InicioFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(InicioViewModel.class);
-        // TODO: Use the ViewModel
     }
-
 }
